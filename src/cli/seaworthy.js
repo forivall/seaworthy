8a1a536c06e5a3229a951e7e28ebbdf1febd8704
Emily Marigold Klassen <forivall@gmail.com>
2019-06-17 14:01:22 -0700

    feat: also check repository

--- src/cli/seaworthy.js
+++ src/cli/seaworthy.js
@@ -11,16 +11,38 @@ export function run(argv) {
 
   let errored = false;
 
-  for (const pattern of grammar.rawPatterns) {
+  if (grammar.rawPatterns.length === 0) {
+    console.log('No patterns found!')
+    errored = true
+  }
+
+  function validatePatterns(patterns) {
+    for (const pattern of patterns) {
+      validateRegexp(pattern, 'match')
+      validateRegexp(pattern, 'begin')
+      validateRegexp(pattern, 'end')
+      if (pattern.patterns) {
+        validatePatterns(pattern.patterns)
+      }
+    }
+  }
+  function validateRegexp(pattern, key) {
+    if (pattern[key] == null) return
+    // console.warn('validate', key, pattern.comment)
     try {
       new onigumura.OnigRegExp(pattern.match); // eslint-disable-line no-new
     } catch (e) {
       errored = true;
-      // TODO: CSON line numbers
+      // TODO: line numbers
       console.log(`\n${pattern.name}${pattern.comment ? ` (${pattern.comment})` : ''}: ${pattern.match}`);
       console.log(e.message);
     }
   }
+  validatePatterns(grammar.rawPatterns)
+  for (const r of Object.keys(grammar.rawRepository)) {
+    validatePatterns(grammar.rawRepository[r].patterns)
+  }
 
   console.log(errored ? '\nSwab the poop deck!' : 'This ship be ready to sail!');
+  if (errored) process.exitCode = 1
 }
